f4cdb676ac43c8f1b2414ba180d6d5f9b9ded179
sou808 <souhajemai867@gmail.com>
2024-10-15 23:49:17 +0100

    Update compt.java

--- compt.java
+++ compt.java
@@ -1,31 +1,52 @@
 package teststatic;
 
+
 public class compt {
-	private int num; 
-	private float solde=0; 
-	private static int compteur=0;
-	public compt() {
-		
-		compteur++; 
-		this.num=compteur;
-		this.solde=0;
-	}
-	public void deposer(float m) {
-           
-			this.solde += m;
-            System.out.println(m + " montant a deposer "+ this.solde);
-       
+    private int num; 
+    private float solde = 0; 
+    private static int compteur = 0;
+
+
+    public compt() {
+        compteur++; 
+        this.num = compteur;
+        this.solde = 0;
     }
- public  void retirer(float m) {
-	 this.solde-=m;
-	 System.out.println(m+"montant a retirer"+this.solde);
- }
- public static float avoirsolde(float solde) {
-	 return solde; 
- }
- public void transferer(float  m,compt c) {
-	 this.retirer(m);
-	 c.deposer(m);
- }
+
+    
+    public void deposer(float m) {
+        this.solde += m;
+        System.out.println( m + " montant a deposer : " + this.solde);
+    }
+
+
+    public void retirer(float m) {
+        if (m <= this.solde) {
+            this.solde -= m;
+            System.out.println( m + " montant a retirer " + this.solde);
+        } else {
+            System.out.println("Retrait impossible  Solde insuffisant.");
+        }
+    }
+
+    
+    public float getSolde() {
+        return this.solde;
+    }
+
+    public void transferer(float m, compt c) {
+        if (m <= this.solde) {
+            this.retirer(m);
+            c.deposer(m);
+            System.out.println("Transfert réussi de " + m + " vers le compte numéro " + c.getNum());
+        } else {
+            System.out.println("Transfert impossible : Solde insuffisant.");
+        }
+    }
+
+    
+    public int getNum() {
+        return this.num;
+    }}
  
 }
