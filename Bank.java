f25148c8bc924737f42753273b5e9b29343f6b7a
sou808 <souhajemai867@gmail.com>
2024-10-15 23:48:43 +0100

    Bank.java

--- Bank.java
+++ Bank.java
@@ -1,10 +1,40 @@
 package teststatic;
 
+
+
 public class Bank {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+       
+        compt co1 = new compt();
+        compt co2 = new compt();
 
-	}
+        
+        System.out.println("Compte 1 Numéro : " + co1.getNum() + " Solde : " + co1.getSolde());
+        System.out.println("Compte 2 Numéro : " + co2.getNum() + " Solde : " + co2.getSolde());
 
+       
+        co1.deposer(1200);
+        co2.deposer(500);
+
+      
+        System.out.println("Après dépot : ");
+        System.out.println("Compte 1 Solde : " + co1.getSolde());
+        System.out.println("Compte 2  Solde : " + co2.getSolde());
+
+       
+        if (co2.getSolde() >= 700) {
+            co2.transferer(700, co1);  
+        } else {
+            System.out.println("Transfert impossible ");
+        }
+
+     
+        System.out.println("Après transfert : ");
+        System.out.println("Compte 1 | Solde : " + co1.getSolde());
+        System.out.println("Compte 2 | Solde : " + co2.getSolde());
+
+      
+    
+    }
 }
